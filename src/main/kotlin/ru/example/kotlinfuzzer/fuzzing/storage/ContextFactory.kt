771045ea9697d0fe03719e5c9eb8bb4ab338efc0
maxim <max-zu99@mail.ru>
2020-04-27 15:38:51 +0300

    Use thread local context

--- src/main/kotlin/ru/example/kotlinfuzzer/fuzzing/storage/ContextFactory.kt
+++ src/main/kotlin/ru/example/kotlinfuzzer/fuzzing/storage/ContextFactory.kt
@@ -3,17 +3,13 @@ package ru.example.kotlinfuzzer.fuzzing.storage
 import ru.example.kotlinfuzzer.cli.CommandLineArgs
 import ru.example.kotlinfuzzer.fuzzing.Fuzzer
 import ru.example.kotlinfuzzer.fuzzing.inputhandlers.Context
-import java.util.concurrent.ConcurrentHashMap
 
 /** Contains contexts for every calling thread. */
 class ContextFactory(private val fuzzer: Fuzzer, private val storage: Storage, private val arguments: CommandLineArgs) {
-    private val contexts = ConcurrentHashMap<Long, Context>()
+    private val localContext: ThreadLocal<Context> = ThreadLocal.withInitial {
+        Context(storage, arguments, fuzzer, this)
+    }
 
     /** Return context unique for calling thread or create a new one. */
-    fun acquire(): Context {
-        val threadId = Thread.currentThread().id
-        return contexts[threadId] ?: Context(storage, arguments, fuzzer, this).also {
-            contexts[threadId] = it
-        }
-    }
+    fun acquire(): Context = localContext.get()
 }
