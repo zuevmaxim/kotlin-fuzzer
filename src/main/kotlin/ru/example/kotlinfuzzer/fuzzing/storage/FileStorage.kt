f2bcdf83c4655c0e99686844fee74e6d43960c03
maxim <max-zu99@mail.ru>
2020-05-14 21:27:37 +0300

    Use one file for crash

--- src/main/kotlin/ru/example/kotlinfuzzer/fuzzing/storage/FileStorage.kt
+++ src/main/kotlin/ru/example/kotlinfuzzer/fuzzing/storage/FileStorage.kt
@@ -4,6 +4,7 @@ import ru.example.kotlinfuzzer.fuzzing.input.ExecutedInput
 import ru.example.kotlinfuzzer.fuzzing.input.FailInput
 import ru.example.kotlinfuzzer.fuzzing.input.Hash
 import java.io.File
+import java.io.FileOutputStream
 import java.io.PrintWriter
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -20,23 +21,21 @@ class FileStorage(workingDirectory: File, name: String) {
     fun save(input: ExecutedInput) = saveInput(input.data, input.hash)
 
     fun save(input: FailInput) {
-        saveInput(input.data, input.hash)
-        val file = File(directory, input.hash.toString() + ".error")
-        if (file.createNewFile()) {
-            count.incrementAndGet()
+        val file = saveInput(input.data, input.hash)
+        PrintWriter(FileOutputStream(file, true)).use { out ->
+            out.println("===END OF INPUT===")
+            input.e.printStackTrace(out)
+            out.println(input.data.joinToString(", ", prefix = "[", postfix = "]") { String.format("0x%02x", it) })
         }
-        PrintWriter(file).use { out -> input.e.printStackTrace(out) }
-        val dataFile = File(directory, input.hash.toString() + ".txt")
-        dataFile.createNewFile()
-        PrintWriter(dataFile).use { out -> out.print(input.data.joinToString(", ", prefix = "[", postfix = "]") { String.format("0x%02x", it) }) }
     }
 
-    internal fun saveInput(data: ByteArray, hash: Hash) {
+    internal fun saveInput(data: ByteArray, hash: Hash): File {
         val file = File(directory, hash.toString())
         if (file.createNewFile()) {
             count.incrementAndGet()
         }
         file.writeBytes(data)
+        return file
     }
 
     fun listFilesContent() = directory.listFiles()?.map { it.readBytes() }
