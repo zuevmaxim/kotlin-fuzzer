5c963af86d34c47631e3276eea515cd242284de1
maxim <max-zu99@mail.ru>
2020-05-21 13:11:05 +0300

    Update test.sh

--- test-project/test.sh
+++ test-project/test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-if [ $# -ne 2 ]; then
-  echo "usage: test CLASS_NAME METHOD_NAME"
+if [ $# -ne 3 ]; then
+  echo "usage: test CLASS_NAME METHOD_NAME PACKAGE"
   exit 1
 fi
 
@@ -9,13 +9,14 @@ cd .. && ./gradlew jar && cd test-project
 ./gradlew jar
 className="ru.example.kotlinfuzzer.tests.$1"
 methodName=$2
+testPackage=$3
 mkdir -p results
 dirName="results/$className.$methodName"
 rm -rf "$dirName"
 mkdir "$dirName"
 java -jar ../build/libs/kotlin-fuzzer-all.jar \
   --classpath build/libs/test-project-1.0-SNAPSHOT-all.jar \
-  --packages ru.example.kotlinfuzzer.tests \
+  --packages ru.example.kotlinfuzzer.tests:"$testPackage" \
   --className "$className" \
   --methodName "$methodName" \
   --workingDirectory "$dirName"
