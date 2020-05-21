4ef1772023f6da8574557d85905e6073d0ca60fa
maxim <max-zu99@mail.ru>
2020-05-21 12:58:06 +0300

    Limit corpus size for composite run

--- test-project/src/main/kotlin/kotlinx/fuzzer/tests/simple/utils/StringTest.kt
+++ test-project/src/main/kotlin/kotlinx/fuzzer/tests/simple/utils/StringTest.kt
@@ -22,4 +22,24 @@ class StringTest {
         localFunction(bytes[0].toInt())
         return 1
     }
+
+    fun compositeCoverage(bytes: ByteArray): Int {
+        val s = String(bytes)
+        if (s.isNotEmpty() && s[0] == 'a') {
+            if (s.length > 1 && s[1] == 'b') {
+                if (s.length > 2 && s[2] == 'c') {
+                    if (s.length > 3 && s[3] == 'd') {
+                        error("Crash")
+                    }
+                }
+            } else if (s.length > 1 && s[1] == 'e') {
+                if (s.length > 2 && s[2] == 'f') {
+                    if (s.length > 3 && s[3] == 'g') {
+                        error("Crash")
+                    }
+                }
+            }
+        }
+        return 1
+    }
 }
