f196887558e647e0f0a4ec0271af7643fe424f40
maxim <max-zu99@mail.ru>
2020-06-24 10:39:11 +0300

    Fixes

--- src/main/kotlin/kotlinx/fuzzer/fuzzing/log/TasksLog.kt
+++ src/main/kotlin/kotlinx/fuzzer/fuzzing/log/TasksLog.kt
@@ -3,6 +3,9 @@ package kotlinx.fuzzer.fuzzing.log
 import java.util.concurrent.ThreadPoolExecutor
 
 class TasksLog(private val threadPool: ThreadPoolExecutor, private val maxTaskQueueSize: Int) {
-    fun queueUsage() = threadPool.queue.size * 100 / maxTaskQueueSize
-    fun completedTasks() = threadPool.completedTaskCount
+    val queueUsage: Int
+        get() = threadPool.queue.size * 100 / maxTaskQueueSize
+
+    val completedTasks: Long
+        get() = threadPool.completedTaskCount
 }
