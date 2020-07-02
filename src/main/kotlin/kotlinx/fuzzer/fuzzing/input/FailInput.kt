9cb5b6a4ed1eb37ea939779dfebe5850a9fa78c1
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-07-02 20:30:21 +0300

    Kotlinizing cleanup

--- src/main/kotlin/kotlinx/fuzzer/fuzzing/input/FailInput.kt
+++ src/main/kotlin/kotlinx/fuzzer/fuzzing/input/FailInput.kt
@@ -4,14 +4,14 @@ import kotlinx.fuzzer.coverage.CoverageRunner
 import kotlinx.fuzzer.fuzzing.TargetMethod
 import kotlinx.fuzzer.fuzzing.inputhandlers.InputMinimizer
 import kotlinx.fuzzer.fuzzing.storage.Storage
-import kotlinx.fuzzer.fuzzing.storage.exceptions.StacktraceEqualException
+import kotlinx.fuzzer.fuzzing.storage.exceptions.*
 
 class FailInput(data: ByteArray, val e: Throwable) : Input(data) {
     /** A minimization is possible if minimized input fails with the same exception. */
     override fun minimize(coverageRunner: CoverageRunner, targetMethod: TargetMethod) =
             InputMinimizer<FailInput>(coverageRunner, targetMethod).minimize(this) { newInput ->
                 when (newInput) {
-                    is FailInput -> StacktraceEqualException.areEqual(this.e, newInput.e)
+                    is FailInput -> e.strackTraceEqualTo(newInput.e)
                     else -> false
                 }
             }
