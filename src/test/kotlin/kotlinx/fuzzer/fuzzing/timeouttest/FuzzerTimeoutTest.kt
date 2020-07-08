306ad2959530fd143646293b287827449feab4d8
Maksim Zuev <max-zu99@mail.ru>
2020-07-08 10:34:03 +0300

    Use single fuzzer context

---
+++ src/test/kotlin/kotlinx/fuzzer/fuzzing/timeouttest/FuzzerTimeoutTest.kt
@@ -0,0 +1,26 @@
+package kotlinx.fuzzer.fuzzing.timeouttest
+
+import kotlinx.fuzzer.Fuzz
+import kotlinx.fuzzer.FuzzCrash
+import kotlinx.fuzzer.Fuzzer
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.File
+
+internal class FuzzerTimeoutTest {
+    @Fuzz("testB")
+    fun fuzz(bytes: ByteArray): Int {
+        return 1
+    }
+
+    @FuzzCrash
+    fun callback(e: Throwable, data: ByteArray) {
+        throw e
+    }
+
+    @Test
+    fun timeoutTest() {
+        Fuzzer(FuzzerTimeoutTest::class.java).start(2)
+        Assertions.assertTrue(File("testB").deleteRecursively())
+    }
+}
