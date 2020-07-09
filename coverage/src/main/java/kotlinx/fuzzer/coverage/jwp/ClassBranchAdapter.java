fe174b01e86488c50f54a65b7767470f555228a4
Maksim Zuev <max-zu99@mail.ru>
2020-07-09 14:06:09 +0300

    Refactor coverage

--- coverage/src/main/java/kotlinx/fuzzer/coverage/jwp/ClassBranchAdapter.java
+++ coverage/src/main/java/kotlinx/fuzzer/coverage/jwp/ClassBranchAdapter.java
@@ -29,32 +29,32 @@ import org.objectweb.asm.*;
 /** The {@link ClassVisitor} that uses {@link MethodBranchAdapter} to insert branch calls in methods */
 class ClassBranchAdapter extends ClassVisitor {
 
-  /** Create new classfile bytecode set from given original classfile bytecode using this adapter */
-  public static byte[] transform(byte[] origBytes) {
-    ClassReader reader = new ClassReader(origBytes);
-    ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_MAXS);
-    reader.accept(new ClassBranchAdapter(BranchTracker.refs, writer), 0);
-    return writer.toByteArray();
-  }
+    private final MethodBranchAdapter.MethodRef ref;
+    private String className;
 
-  private final MethodBranchAdapter.MethodRefs refs;
-  private String className;
+    /** Create this adapter with the given {@link MethodBranchAdapter.MethodRef} to call */
+    public ClassBranchAdapter(MethodBranchAdapter.MethodRef ref, ClassVisitor cv) {
+        super(Opcodes.ASM6, cv);
+        this.ref = ref;
+    }
 
-  /** Create this adapter with the given {@link MethodBranchAdapter.MethodRefs} to call */
-  public ClassBranchAdapter(MethodBranchAdapter.MethodRefs refs, ClassVisitor cv) {
-    super(Opcodes.ASM6, cv);
-    this.refs = refs;
-  }
+    /** Create new classfile bytecode set from given original classfile bytecode using this adapter */
+    public static byte[] transform(byte[] origBytes) {
+        ClassReader reader = new ClassReader(origBytes);
+        ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_MAXS);
+        reader.accept(new ClassBranchAdapter(BranchTracker.ref, writer), 0);
+        return writer.toByteArray();
+    }
 
-  @Override
-  public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-    super.visit(version, access, name, signature, superName, interfaces);
-    className = name;
-  }
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        super.visit(version, access, name, signature, superName, interfaces);
+        className = name;
+    }
 
-  @Override
-  public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-    MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
-    return new MethodBranchAdapter(refs, className, access, name, desc, signature, exceptions, mv);
-  }
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
+        return new MethodBranchAdapter(ref, className, access, name, desc, signature, exceptions, mv);
+    }
 }
