ed107b6f541660f6a64f7ac7859aa181f578346a
Maksim Zuev <max-zu99@mail.ru>
2020-07-09 14:06:09 +0300

    Refactor coverage

--- coverage/src/main/java/kotlinx/fuzzer/coverage/jwp/Tracer.java
+++ coverage/src/main/java/kotlinx/fuzzer/coverage/jwp/Tracer.java
@@ -24,39 +24,32 @@
 
 package kotlinx.fuzzer.coverage.jwp;
 
-import java.util.Arrays;
-import java.util.Map;
-
 /** Base interface for all tracers. The primary implementation is {@link Instrumenting}. */
 interface Tracer {
 
-  /** Start a trace on the given thread. This should throw if already being traced. */
-  void startTrace(Thread thread);
+    /** Start a trace on current thread. This should throw if already being traced. */
+    void startTrace();
 
-  /**
-   * Stop the trace on the given thread. If there is not a trace on the given thread, this should return null. The
-   * resulting array from this trace should be sorted using {@link BranchHit}'s set ordering.
-   */
-  BranchHit[] stopTrace(Thread thread);
+    /** Stop the trace on current thread. If there is not a trace on the given thread, this should return null. */
+    int[] stopTrace();
 
-  /** Main tracer using instrumenting */
-  class Instrumenting implements Tracer {
-    @Override
-    public void startTrace(Thread thread) {
-      BranchTracker.beginTrackingForThread(thread);
+    /** Main tracer using instrumenting */
+    class Instrumenting implements Tracer {
+        @Override
+        public void startTrace() {
+            BranchTracker.beginTrackingForThread();
+        }
+
+        @Override
+        public int[] stopTrace() {
+            BranchTracker.BranchHits hits = BranchTracker.endTrackingForThread();
+            if (hits == null) return null;
+            int[] ret = new int[hits.branchHashHits.size()];
+            int index = 0;
+            for (Integer hitHash : hits.branchHashHits) {
+                ret[index++] = hitHash;
+            }
+            return ret;
+        }
     }
-
-    @Override
-    public BranchHit[] stopTrace(Thread thread) {
-      BranchTracker.BranchHits hits = BranchTracker.endTrackingForThread(thread);
-      if (hits == null) return null;
-      BranchHit[] ret = new BranchHit[hits.branchHashHits.size()];
-      int index = 0;
-      for (Map.Entry<Integer, BranchTracker.IntRef> hit : hits.branchHashHits.entrySet()) {
-        ret[index++] = new BranchHit(hit.getKey(), hit.getValue().value);
-      }
-      Arrays.sort(ret);
-      return ret;
-    }
-  }
 }
