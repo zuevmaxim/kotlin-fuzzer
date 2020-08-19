df189bc8893b86457acba64e64d6c32b9025d9e2
Maksim Zuev <max-zu99@mail.ru>
2020-08-19 16:53:54 +0300

    SafeClassWriter style fixes

--- coverage/src/main/java/kotlinx/fuzzer/coverage/jwp/SafeClassWriter.java
+++ coverage/src/main/java/kotlinx/fuzzer/coverage/jwp/SafeClassWriter.java
@@ -28,12 +28,15 @@
  */
 package kotlinx.fuzzer.coverage.jwp;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A ClassWriter that computes the common super class of two classes without
@@ -41,10 +44,10 @@ import org.objectweb.asm.Opcodes;
  *
  * @author Eric Bruneton
  */
-public class SafeClassWriter extends ClassWriter {
+class SafeClassWriter extends ClassWriter {
 
     private final ClassLoader loader;
-
+    private static final String OBJECT = "java/lang/Object";
 
     public SafeClassWriter(ClassReader cr, ClassLoader loader, final int flags) {
         super(cr, flags);
@@ -56,100 +59,86 @@ public class SafeClassWriter extends ClassWriter {
         try {
             ClassReader info1 = typeInfo(type1);
             ClassReader info2 = typeInfo(type2);
-            if ((info1.getAccess() & Opcodes.ACC_INTERFACE) != 0) {
-                if (typeImplements(type2, info2, type1)) {
-                    return type1;
-                } else {
-                    return "java/lang/Object";
-                }
+            if (isInterface(info1)) {
+                return getCommonSuperClassWithInterface(type1, type2, info2);
             }
-            if ((info2.getAccess() & Opcodes.ACC_INTERFACE) != 0) {
-                if (typeImplements(type1, info1, type2)) {
-                    return type2;
-                } else {
-                    return "java/lang/Object";
-                }
+            if (isInterface(info2)) {
+                return getCommonSuperClassWithInterface(type2, type1, info1);
             }
-            StringBuilder b1 = typeAncestors(type1, info1);
-            StringBuilder b2 = typeAncestors(type2, info2);
-            String result = "java/lang/Object";
-            int end1 = b1.length();
-            int end2 = b2.length();
-            while (true) {
-                int start1 = b1.lastIndexOf(";", end1 - 1);
-                int start2 = b2.lastIndexOf(";", end2 - 1);
-                if (start1 != -1 && start2 != -1
-                        && end1 - start1 == end2 - start2) {
-                    String p1 = b1.substring(start1 + 1, end1);
-                    String p2 = b2.substring(start2 + 1, end2);
-                    if (p1.equals(p2)) {
-                        result = p1;
-                        end1 = start1;
-                        end2 = start2;
-                    } else {
-                        return result;
-                    }
+            List<String> ancestors1 = typeAncestors(type1, info1);
+            List<String> ancestors2 = typeAncestors(type2, info2);
+            String result = OBJECT;
+            for (int i = 0; i < Math.min(ancestors1.size(), ancestors2.size()); i++) {
+                if (ancestors1.get(i).equals(ancestors2.get(i))) {
+                    result = ancestors1.get(i);
                 } else {
                     return result;
                 }
             }
+            return result;
         } catch (IOException e) {
             throw new RuntimeException(e.toString());
         }
     }
 
+    private String getCommonSuperClassWithInterface(String interfaceType, String type, ClassReader typeInfo) throws IOException {
+        if (typeImplements(type, typeInfo, interfaceType)) {
+            return interfaceType;
+        } else {
+            return OBJECT;
+        }
+    }
+
+    private boolean isInterface(ClassReader info) {
+        return (info.getAccess() & Opcodes.ACC_INTERFACE) != 0;
+    }
+
     /**
      * Returns the internal names of the ancestor classes of the given type.
      *
-     * @param type
-     *            the internal name of a class or interface.
-     * @param info
-     *            the ClassReader corresponding to 'type'.
-     * @return a StringBuilder containing the ancestor classes of 'type',
-     *         separated by ';'. The returned string has the following format:
-     *         ";type1;type2 ... ;typeN", where type1 is 'type', and typeN is a
-     *         direct subclass of Object. If 'type' is Object, the returned
-     *         string is empty.
-     * @throws IOException
-     *             if the bytecode of 'type' or of some of its ancestor class
-     *             cannot be loaded.
+     * @param type the internal name of a class or interface.
+     * @param info the ClassReader corresponding to 'type'.
+     * @return a List containing the ancestor classes of 'type'.
+     * The returned list contains types:
+     * type1,type2 ... ,typeN, where typeN is 'type', and type1 is a
+     * direct subclass of Object. If 'type' is Object, the returned
+     * list is empty.
+     * @throws IOException if the bytecode of 'type' or of some of its ancestor class
+     *                     cannot be loaded.
      */
-    private StringBuilder typeAncestors(String type, ClassReader info)
+    private List<String> typeAncestors(String type, ClassReader info)
             throws IOException {
-        StringBuilder b = new StringBuilder();
-        while (!"java/lang/Object".equals(type)) {
-            b.append(';').append(type);
+        List<String> result = new ArrayList<>();
+        while (!type.equals(OBJECT)) {
+            result.add(type);
             type = info.getSuperName();
             info = typeInfo(type);
         }
-        return b;
+        Collections.reverse(result);
+        return result;
     }
 
     /**
      * Returns true if the given type implements the given interface.
      *
-     * @param type
-     *            the internal name of a class or interface.
-     * @param info
-     *            the ClassReader corresponding to 'type'.
-     * @param itf
-     *            the internal name of a interface.
-     * @return true if 'type' implements directly or indirectly 'itf'
-     * @throws IOException
-     *             if the bytecode of 'type' or of some of its ancestor class
-     *             cannot be loaded.
+     * @param type        the internal name of a class or interface.
+     * @param info        the ClassReader corresponding to 'type'.
+     * @param anInterface the internal name of a interface.
+     * @return true if 'type' implements directly or indirectly 'anInterface'
+     * @throws IOException if the bytecode of 'type' or of some of its ancestor class
+     *                     cannot be loaded.
      */
-    private boolean typeImplements(String type, ClassReader info, String itf)
+    private boolean typeImplements(String type, ClassReader info, String anInterface)
             throws IOException {
-        while (!"java/lang/Object".equals(type)) {
-            String[] itfs = info.getInterfaces();
-            for (int i = 0; i < itfs.length; ++i) {
-                if (itfs[i].equals(itf)) {
+        while (!type.equals(OBJECT)) {
+            String[] interfaces = info.getInterfaces();
+            for (String s : interfaces) {
+                if (s.equals(anInterface)) {
                     return true;
                 }
             }
-            for (int i = 0; i < itfs.length; ++i) {
-                if (typeImplements(itfs[i], typeInfo(itfs[i]), itf)) {
+            for (String s : interfaces) {
+                if (typeImplements(s, typeInfo(s), anInterface)) {
                     return true;
                 }
             }
@@ -162,22 +151,17 @@ public class SafeClassWriter extends ClassWriter {
     /**
      * Returns a ClassReader corresponding to the given class or interface.
      *
-     * @param type
-     *            the internal name of a class or interface.
+     * @param type the internal name of a class or interface.
      * @return the ClassReader corresponding to 'type'.
-     * @throws IOException
-     *             if the bytecode of 'type' cannot be loaded.
+     * @throws IOException if the bytecode of 'type' cannot be loaded.
      */
     private ClassReader typeInfo(final String type) throws IOException {
         String resource = type + ".class";
-        InputStream is = loader.getResourceAsStream(resource);
-        if (is == null) {
-            throw new IOException("Cannot create ClassReader for type " + type);
-        }
-        try {
+        try (InputStream is = loader.getResourceAsStream(resource)) {
+            if (is == null) {
+                throw new IOException("Cannot create ClassReader for type " + type);
+            }
             return new ClassReader(is);
-        } finally {
-            is.close();
         }
     }
 }
