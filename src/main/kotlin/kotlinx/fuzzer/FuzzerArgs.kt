57c68f921d7a59c02f350badb3be459f4d28db52
Maksim Zuev <max-zu99@mail.ru>
2020-08-21 13:32:14 +0300

    Use work stealing pool

--- src/main/kotlin/kotlinx/fuzzer/FuzzerArgs.kt
+++ src/main/kotlin/kotlinx/fuzzer/FuzzerArgs.kt
@@ -8,7 +8,6 @@ data class FuzzerArgs(
     val workingDirectory: String,
     val classpath: List<String> = emptyList(),
     val packages: List<String>,
-    val maxTaskQueueSize: Int = Fuzzer.MAX_TASK_QUEUE_SIZE,
     val threadsNumber: Int = Runtime.getRuntime().availableProcessors(),
     val storageStrategy: StorageStrategy? = null,
     val maxCorpusSize: Int = Fuzzer.MAX_CORPUS_SIZE,
@@ -16,7 +15,6 @@ data class FuzzerArgs(
 ) {
     init {
         require(threadsNumber >= 1) { "Number of threads should be at least one." }
-        require(maxTaskQueueSize >= 1) { "Size of queue should be at least one." }
         require(maxCorpusSize >= 1) { "Corpus size should be at least one." }
     }
 }
