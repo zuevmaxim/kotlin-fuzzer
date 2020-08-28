f549395a279851ef2f14c057e7712d2aabfa134e
zuevmaxim <max-zu99@mail.ru>
2020-08-28 16:58:42 +0300

    Limit minimization time by 1 second (#37)
    * Limit minimization time by 1 second
    
    * Add minimization test
    
    * Fixes
    
    * Add comment

--- src/main/kotlin/kotlinx/fuzzer/fuzzing/inputhandlers/minimization.kt
+++ src/main/kotlin/kotlinx/fuzzer/fuzzing/inputhandlers/minimization.kt
@@ -4,8 +4,10 @@ package kotlinx.fuzzer.fuzzing.inputhandlers
  * Tries to drop a range of bytes. Uses greedy algorithm.
  * Produces O((size of [array])^2) [isValidArray] executions.
  * Assumes that [isValidArray] returns true on [array].
+ * @param timeoutMs timeout for minimization execution(as minimization could run too long time and slow down fuzzer)
  */
-inline fun minimizeArray(array: ByteArray, isValidArray: (ByteArray) -> Boolean): ByteArray {
+inline fun minimizeArray(array: ByteArray, isValidArray: (ByteArray) -> Boolean, timeoutMs: Long): ByteArray {
+    val start = System.currentTimeMillis()
     assert(isValidArray(array))
     var currentArray = array
     for (dropLength in array.size downTo 1) {
@@ -13,6 +15,9 @@ inline fun minimizeArray(array: ByteArray, isValidArray: (ByteArray) -> Boolean)
         var candidate = ByteArray(currentArray.size - dropLength)
         var dropIndex = 0
         while (dropIndex <= currentArray.size - dropLength) {
+            if (System.currentTimeMillis() - start > timeoutMs) {
+                return currentArray
+            }
             if (candidate.size != currentArray.size - dropLength) {
                 candidate = ByteArray(currentArray.size - dropLength)
             }
@@ -29,3 +34,9 @@ inline fun minimizeArray(array: ByteArray, isValidArray: (ByteArray) -> Boolean)
     }
     return currentArray
 }
+
+inline fun minimizeArray(array: ByteArray, isValidArray: (ByteArray) -> Boolean) =
+    minimizeArray(array, isValidArray, DEFAULT_TIMEOUT_MS)
+
+/** Default minimization timeout is 1 second. */
+const val DEFAULT_TIMEOUT_MS = 1000L
