4318716c9074dbb6408e35cb1ab26d0edd770b90
maxim <max-zu99@mail.ru>
2020-09-01 23:25:51 +0300

    Implement throw strategy

---
+++ src/test/kotlin/kotlinx/fuzzer/fuzzing/annotationtest/ThrowStrategyTest.kt
@@ -0,0 +1,25 @@
+package kotlinx.fuzzer.fuzzing.annotationtest
+
+import kotlinx.fuzzer.Fuzz
+import kotlinx.fuzzer.Fuzzer
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.File
+
+
+internal class ThrowStrategyTest {
+    @Fuzz("testD")
+    fun fuzz(bytes: ByteArray): Int {
+        check(bytes.size < 5)
+        return 1
+    }
+
+    @Test
+    fun testThrowsFuzzMethodException() {
+        assertThrows<IllegalStateException> {
+            Fuzzer(ThrowStrategyTest::class.java).start()
+        }
+        assertTrue(File("testD").deleteRecursively())
+    }
+}
