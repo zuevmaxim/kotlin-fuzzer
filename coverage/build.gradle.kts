b73820afb1753186dc27c083d216ba434764706a
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2024-04-16 13:45:24 +0200

    Basic update of Gradle

--- coverage/build.gradle.kts
+++ coverage/build.gradle.kts
@@ -1,9 +1,21 @@
+import kotlinx.fuzzer.publish.mavenCentralMetadata
+import kotlinx.fuzzer.publish.publishBintray
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.kotlin.gradle.tasks.*
+import java.util.*
+
+
 plugins {
+    kotlin("jvm") version "1.9.20"
     java
+    application
+    `maven-publish`
 }
 
 repositories {
     mavenCentral()
+
 }
 
 dependencies {
@@ -13,4 +25,14 @@ dependencies {
     implementation("org.ow2.asm:asm-util:$asmVersion")
 
     implementation("org.jetbrains:annotations:20.0.0")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            mavenCentralMetadata()
+        }
+        mavenCentralMetadata()
+    }
 }
--- test-project/build.gradle.kts
+++ test-project/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.3.61"
-    id("org.springframework.boot") version "2.2.7.RELEASE"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    kotlin("jvm") version "1.9.20"
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
     id("java")
 }
 
@@ -10,16 +10,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     // ktor
-    implementation("io.ktor:ktor-server-netty:1.3.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
-    implementation("com.athaydes.rawhttp:rawhttp-core:2.2.2")
+//    implementation("io.ktor:ktor-server-netty:1.3.2")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+//    implementation("com.athaydes.rawhttp:rawhttp-core:2.2.2")
 
     // apache
     implementation("org.apache.httpcomponents:httpclient:4.5")
@@ -44,7 +43,7 @@ tasks {
         archiveClassifier.set("all")
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
         manifest { }
-        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+//        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
         from(sourceSets.main.get().output)
     }
 }
